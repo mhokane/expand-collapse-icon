636ea74f0c71e409f7bde8a1e6e9f8e38113f5f7
mhokane <mhokane@mhk-3.lcinc.org>
2014-12-30 14:08:50 -0500

    Adding css animation, and cols to display diff versions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   
-  // Write your Javascript!
+  // if this doesn't have the class 'click', toggle 'hover' class and remove 'hover--clicked'
+  // 'hover--clicked' is bringing you back to the default state once you've closed and still have the mouse over the icon
+  $('.svg-wrap--ex-col').hover(function() {
+    if (!$(this).hasClass('click')) {
+      $(this).toggleClass('hover');
+      $(this).removeClass('hover--clicked')
+    };
+  });
+
+  // on click toggle 'click' class, if 'hover' class then remove 'hover' class
+  $('.svg-wrap--ex-col').click(function() {
+    $(this).toggleClass('click').toggleClass('hover');
+    if ($(this).hasClass('hover')) {
+      $(this).toggleClass('hover--clicked');
+    };
+  });
 
 });
